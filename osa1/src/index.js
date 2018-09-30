7d052cc7bdb8f3fe0a685829988972c0bd0ac398
squigothic <squigothic@gmail.com>
2018-09-30 22:11:09 +0300

    Tehtävät 1.3-1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,19 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 ='Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const kurssi = {
+    nimi: 'Half stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteeet',
+        tehtavia: 10,
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ],
+  }
 
   return (
     <div>
-      <Otsikko kurssi={kurssi} />
-      <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
-      <p>Yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+      <Otsikko kurssi={kurssi.nimi} />
+      <Sisalto osat={kurssi.osat} />
+      <p>Yhteensä {kurssi.osat[0].tehtavia + kurssi.osat[1].tehtavia + kurssi.osat[2].tehtavia} tehtävää</p>
     </div>
   )
 }
@@ -25,19 +35,19 @@ const Otsikko = ({ kurssi }) => {
   )
 }
 
-const Sisalto = ({ osa1, osa2, osa3, tehtavia1, tehtavia2, tehtavia3 }) => {
+const Sisalto = ({osat}) => {
   return (
     <div>
-      <Osa osa={osa1} tehtavia={tehtavia1} />
-      <Osa osa={osa2} tehtavia={tehtavia2} />
-      <Osa osa={osa3} tehtavia={tehtavia3} />
+      <Osa osa={osat[0]} />
+      <Osa osa={osat[1]} />
+      <Osa osa={osat[2]} />
     </div>
   )
 }
 
-const Osa = ({osa, tehtavia}) => {
+const Osa = ({osa: {nimi, tehtavia}}) => {
   return (
-    <p>{osa} {tehtavia}</p>
+    <p>{nimi} {tehtavia}</p>
   )
 }
 
