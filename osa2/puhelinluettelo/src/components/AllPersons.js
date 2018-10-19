535ec87f97f8b8a79f4eaeced9634461a8a75774
Teemu Laitila <squigothic@gmail.com>
2018-10-19 23:29:47 +0300

    vähennetty debuggausviestejä

--- osa2/puhelinluettelo/src/components/AllPersons.js
+++ osa2/puhelinluettelo/src/components/AllPersons.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Person from './Person'
 
 const AllPersons = ({ personList, filter }) => {
-  console.log('suodin on: ', filter)
   if (personList.length === 0) {
     return 'Ei henkilöitä listalla'
   }
   const personsToDisplay = personList
     .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-  console.log('esitettävät:', personsToDisplay)
+
   return (
     <div>
         {personsToDisplay
