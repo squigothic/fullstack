99271f606eae3802ab5dbda0092ffb85f8d95c51
Teemu Laitila <squigothic@gmail.com>
2018-10-22 22:40:22 +0300

    initial commit

---
+++ osa2/maidentiedot/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      maat: [],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Maa-ohjelma</h1>
+      </div> 
+    )
+  }
+}
+
+export default App;
