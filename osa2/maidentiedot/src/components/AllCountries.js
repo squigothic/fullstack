3f487528e08183d43b4805de4b056934be78f823
Teemu Laitila <squigothic@gmail.com>
2018-10-23 23:11:12 +0300

    sovellus sisältää vähemmän debuggausviestejä

--- osa2/maidentiedot/src/components/AllCountries.js
+++ osa2/maidentiedot/src/components/AllCountries.js
@@ -5,26 +5,19 @@ const AllCountries = ({ countryList, filter, handleClick }) => {
   const countriesToDisplay = countryList
     .filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
 
-  const handleFilterChange = (newFilter) => {
-    handleClick(newFilter)
-  }
-
   if(countriesToDisplay.length === 1 && filter !== '') {
-    console.log(countriesToDisplay)
     return (
       <div>
         <DisplayCountry country={countriesToDisplay[0]} />
       </div>
     )
   } else if(countriesToDisplay.length > 10) {
-    console.log('Maita oli yli kymmenen')
     return (
       <div>
         Too many matches, specify another filter
       </div>
     )
   } else {
-    console.log('maita oli alle kymmenen mutta yli yksi tai taulukko oli tyhjä')
     return (
       <div>
           {countriesToDisplay
