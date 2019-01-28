1bdfeb9d6c6cf65eff1333d5c7113407fc0961e5
Teemu Laitila <squigothic@gmail.com>
2019-01-28 20:52:27 +0200

    moved controller to its own directory

--- osa4/app.js
+++ osa4/app.js
@@ -2,13 +2,10 @@ const config = require('./utils/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-//const blogsRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const cors = require('cors')
-//väliaikainen alla
-const Blog = require('./models/blog')
-
 
 console.log('connecting to ', config.MONGODB_URI)
 
@@ -25,25 +22,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
-//app.use('/api/blogs', blogsRouter)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
