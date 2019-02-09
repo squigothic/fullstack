fa89defccb5b7dbccb0550be70b86090fab6c0bf
Teemu Laitila <squigothic@gmail.com>
2019-02-09 10:54:32 +0200

    added route for adding users

--- osa4/models/user.js
+++ osa4/models/user.js
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toStrin()
+    returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
     delete returnedObject.passwordHash
