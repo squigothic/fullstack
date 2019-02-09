47c77318b6316084a4e0ea3c37117da4369f1ff1
Teemu Laitila <squigothic@gmail.com>
2019-02-09 13:59:46 +0200

    added error handling for invalid token

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -13,14 +13,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  if (process.env.NODE_ENV !== 'test') {
-    logger.error(error.message)
-  }
-
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  }
+
+  if (process.env.NODE_ENV !== 'test') {
+    logger.error(error.message)
   }
 
   next(error)
