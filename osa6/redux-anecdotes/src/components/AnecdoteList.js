e1928d50ce68dd4780c73522336d6d618911683a
Teemu <squigothic@gmail.com>
2019-06-11 21:22:38 +0300

    exercise 6.15 done

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -14,21 +14,11 @@ const AnecdoteList = (props) => {
     setTimeout(() => props.hideNotification(), 3000)
   }
 
-
-  let anecdotesToDisplay = {}
-  if (props.filter.length > 0) {
-    const filter = props.filter.toLowerCase()
-    anecdotesToDisplay = props.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter))
-  } else {
-    anecdotesToDisplay = props.anecdotes
-  }
-
-
   return (
     <div>
       <h2>Anecdotes</h2>
       <Filter />
-      {anecdotesToDisplay.sort((a, b) => b.votes - a.votes).map(anecdote =>
+      {props.visibleAnecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -44,10 +34,18 @@ const AnecdoteList = (props) => {
   )
 }
 
+const anecdotesToDisplay = ({ anecdotes, filter }) => {
+  console.log(anecdotes, filter)
+  if (filter.length > 0) {
+    return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+  } else {
+    return anecdotes
+  }
+}
+
 const mapStateToProps = (state) => {
   return {
-    anecdotes: state.anecdotes,
-    filter: state.filter
+    visibleAnecdotes: anecdotesToDisplay(state)
   }
 }
 
