1726acf97ff72c02641fda7fcbdf12480397edfb
Teemu <squigothic@gmail.com>
2019-07-05 22:24:16 +0300

    exercise 6.16 done

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 const App = (props) => {
+  useEffect(() => {
+    anecdoteService.getAll().then(notes => props.initializeAnecdotes(notes))
+  }, [])
+
   return (
     <div>
       <h1>Programming anecdotes</h1>
@@ -14,4 +21,4 @@ const App = (props) => {
   )
 }
 
-export default App
+export default connect(null, { initializeAnecdotes })(App)
